670b99e09596c855c6c2c6e2fc593f7dcd7a17d3
Ololape <olajumokeololape@gmail.com>
2021-05-21 16:36:47 +0300

    more javascript included

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function displayTemperature(response) {
     console.log(response.data);
-    let temperatureElement = document.querySelector("#tempearture");
-    
+    let temperatureElement = document.querySelector("#temperture");
+    let cityElement = document.querySelector(response.main.name);
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name
 }
 let apiKey = "1c72bf446097ca5cb1d7b9793383b20f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Doha&appid=${apiKey}&units=metric`;
-
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
