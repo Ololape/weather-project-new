0162dee4e600fb7b39142b9366d0ceaebb2f4153
Ololape <olajumokeololape@gmail.com>
2021-05-22 21:44:11 +0300

    icon added in real time

--- src/index.js
+++ src/index.js
@@ -20,12 +20,17 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.main.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt * 1000) 
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+    iconElement.setAttribute(
+        "alt", response.data.weather[0].description
+    );
 }
 let apiKey = "1c72bf446097ca5cb1d7b9793383b20f";
 let city = "Doha";
