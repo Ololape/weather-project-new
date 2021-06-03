404d39e7275eb10fab69852f7edce49fee41122b
Ololape <olajumokeololape@gmail.com>
2021-06-04 02:57:19 +0300

    Integrated weather forecast API

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
     
     let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -44,6 +45,12 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
 
 }
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "1c72bf446097ca5cb1d7b9793383b20f";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
     console.log(response.data);
@@ -67,6 +74,7 @@ function displayTemperature(response) {
     iconElement.setAttribute(
         "alt", response.data.weather[0].description
     );
+    getForecast(response.data.coord);
 }
 function search(city) {
     let apiKey = "1c72bf446097ca5cb1d7b9793383b20f";
@@ -109,4 +117,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Doha");
-displayForecast();
